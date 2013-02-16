e1ce2e27be6ed033cfc86d3c260bfe5731764943
ganeshredcobra <ganeshredcobra@gmail.com>
2013-02-16 17:56:44 +0530

    Identify remote code

---
+++ gesture_tv/remote_identify/remote_identify.ino
@@ -0,0 +1,88 @@
+/* YourDuino.com Example Software Sketch
+ IR Remote Kit Test
+ Uses YourDuino.com IR Infrared Remote Control Kit 2
+ http://arduino-direct.com/sunshop/index.php?l=product_detail&p=153
+ based on code by Ken Shirriff - http://arcfn.com
+ Get Library at: https://github.com/shirriff/Arduino-IRremote
+ Unzip folder into Libraries. RENAME folder IRremote
+ terry@yourduino.com */
+
+/*-----( Import needed libraries )-----*/
+
+#include "IRremote.h"
+
+/*-----( Declare Constants )-----*/
+int receiver = 11; // pin 1 of IR receiver to Arduino digital pin 11
+
+/*-----( Declare objects )-----*/
+IRrecv irrecv(receiver);           // create instance of 'irrecv'
+decode_results results;            // create instance of 'decode_results'
+/*-----( Declare Variables )-----*/
+
+
+void setup()   /*----( SETUP: RUNS ONCE )----*/
+{
+  Serial.begin(9600);
+  Serial.println("IR Receiver Raw Data + Button Decode Test");
+  irrecv.enableIRIn(); // Start the receiver
+
+}/*--(end setup )---*/
+
+
+void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+  if (irrecv.decode(&results)) // have we received an IR signal?
+
+  {
+    Serial.println(results.value, HEX);  
+    translateIR(); 
+    irrecv.resume(); // receive the next value
+  }  
+}/* --(end main loop )-- */
+
+/*-----( Declare User-written Functions )-----*/
+void translateIR() // takes action based on IR code received
+
+// describing Car MP3 IR codes 
+
+{
+
+  switch(results.value)
+
+  {
+
+   case 0x80BFA15E:  
+    Serial.println(" CH+            "); 
+    break;
+
+  case 0x80BF619E:  
+    Serial.println(" CH-          "); 
+    break;
+
+  case 0xFF02FD:  
+    Serial.println(" NEXT           "); 
+    break;
+
+  case 0xFFC23D:  
+    Serial.println(" PLAY/PAUSE     "); 
+    break;
+
+  case 0xFFE01F:  
+    Serial.println(" VOL-           "); 
+    break;
+
+
+  default: 
+    Serial.println(" other button   ");
+
+  }
+
+  delay(500);
+
+
+} //END translateIR
+
+
+
+/* ( THE END ) */
+
